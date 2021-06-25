be8371a61e7d1d868805f59ae525221b2fabfd42
geek9012 <84989098+geek9012@users.noreply.github.com>
2021-06-25 13:12:27 +0530

    Update text_sum.py

--- text_sum.py
+++ text_sum.py
@@ -4,4 +4,4 @@ summarizer = pipeline("summarization")
 user_input = st.text_input("Put text here", "")
 if st.button('Summarize'):
   result = summarizer(user_input, max_length=300, min_length=30, do_sample=False)
-  st.write(result)
+  st.write(result[0]["summary_text"])
