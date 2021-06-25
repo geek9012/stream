7cc068b7c1901f4a747532d15a5573ad8ae90276
geek9012 <84989098+geek9012@users.noreply.github.com>
2021-06-25 13:11:07 +0530

    Update text_sum.py

--- text_sum.py
+++ text_sum.py
@@ -3,5 +3,5 @@ from transformers import pipeline
 summarizer = pipeline("summarization")
 user_input = st.text_input("Put text here", "")
 if st.button('Summarize'):
-  result = summarizer(user_input, max_length=130, min_length=30, do_sample=False)
+  result = summarizer(user_input, max_length=300, min_length=30, do_sample=False)
   st.write(result)
