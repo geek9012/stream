0476af2f33f3446f930f6d6a7fb8cf4883b6516d
geek9012 <84989098+geek9012@users.noreply.github.com>
2021-06-25 13:01:19 +0530

    Update text_sum.py

--- text_sum.py
+++ text_sum.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 summarizer = pipeline("summarization")
-user_input = st.text_input("label goes here", "Text to be summarized")
-result = summarizer(user_input, max_length=130, min_length=30, do_sample=False)
-st.write(result)
+user_input = st.text_input("Put text here", "")
+if st.button('Summarize'):
+  result = summarizer(user_input, max_length=130, min_length=30, do_sample=False)
+  st.write(result)
